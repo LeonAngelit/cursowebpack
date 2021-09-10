2ce7f5dffa2ba409a17e1ff492ed8ca1bb9500ee
LeonAngelit <leo63582@gmail.com>
2021-09-10 22:20:53 +0200

    first commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,79 @@
+const { resolve } = require('path');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack')
+
+module.exports = {
+    entry: '/src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: 'assets/hash/[hash][ext][query]'
+    },
+    mode: 'development',
+    watch: true,
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+         '@utils': resolve(__dirname, 'src/utils/'),
+         '@templates': resolve(__dirname, 'src/templates/'),
+         '@styles': resolve(__dirname, 'src/styles/'),
+         '@images': resolve(__dirname, 'src/assets/images/'),
+         '@fonts': resolve(__dirname, 'src/assets/fonts/')
+        }
+    },
+    module: {
+        rules:[
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use:{
+              loader:'babel-loader'
+          }
+        },
+        {
+            test: /\.css|.styl$/i,
+            use: [MiniCssExtractPlugin.loader,'css-loader', 'stylus-loader']
+        },
+        {
+            test: /\.png/,
+            type: 'asset/resource'
+        },
+        {
+            test: /\.woff$/,
+            use: {
+                loader: "url-loader",
+                options: {
+                    limit: 10000,
+                    mimetype: "application/font-woff",
+                    name: "[name].[contenthash].[ext]",
+                    outputPath: "./assets/fonts/",
+                    publicPath: "../assets/fonts/",
+                    esModule: false,
+                }
+            }
+        },
+      ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].[contenthash].css'
+        }),
+        new CopyPlugin({
+            patterns:[
+                {
+                    from: resolve('./src/assets/images/'),
+                    to: 'assets/images'
+                }]
+        }),
+        new Dotenv(),
+    ],
+    
+}
